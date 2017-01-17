fa87f24880c3bb1cdf63061a0d036419f20efcd1
Stefan Liute <stefanliute@gmail.com>
2017-01-17 17:32:14 +0000

    Viewing hit points

--- spec/features/fillinnames_spec.rb
+++ spec/features/fillinnames_spec.rb
@@ -10,3 +10,13 @@ feature "Name input" do
     expect(page).to have_content "It's Sam vs. Stefan!"
   end
 end
+
+feature "Hit points" do
+  scenario "Player 1 sees Player 2's hit points" do
+    visit('/')
+    fill_in :name_01, :with => "Sam"
+    fill_in :name_02, :with => "Stefan"
+    click_button "submit"
+    expect(page).to have_content "Stefan: 100 hit points"
+  end
+end
