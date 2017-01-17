b959b1cca889e141d8fe373dd7b04facf34244c2
Sam Mckay <sammckay12@gmail.com>
2017-01-17 17:16:39 +0000

    storing names as session

--- app.rb
+++ app.rb
@@ -2,13 +2,22 @@ require 'sinatra'
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @name_01 = params[:name_01]
-    @name_02 = params[:name_02]
+    session[:name_01] = params[:name_01]
+    session[:name_02] = params[:name_02]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @name_01 = session[:name_01]
+    @name_02 = session[:name_02]
     erb :play
   end
 
